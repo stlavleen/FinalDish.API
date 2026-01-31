8c2e4e3797aa0ee2e36ec2aa93066f126f94b71a
stlavleen <stanislav.zaynullin@outlook.com>
2026-01-31 23:49:05 +0300

    Db context created and configured, first migration added

--- FinalDish.API/Models/Dish.cs
+++ FinalDish.API/Models/Dish.cs
@@ -1,11 +1,22 @@
-﻿namespace FinalDish.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalDish.API.Models
 {
     public class Dish
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+        
+        [Required]
         public string Name { get; set; } = null!;
+
         public string? Description { get; set; }
+
+        [Required]
         public string Content { get; set; } = null!;
+
+        [Required]
         public DishType Type { get; set; }
     }
 }
--- FinalDish.API/Program.cs
+++ FinalDish.API/Program.cs
@@ -1,4 +1,7 @@
 
+using FinalDish.API.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace FinalDish.API
 {
     public class Program
@@ -13,6 +16,10 @@ namespace FinalDish.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<ApplicationDbContext>(options => 
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
