a810b46c9a5ec380b0c36ee837e122255a327f49
stlavleen <stanislav.zaynullin@outlook.com>
2026-02-05 23:45:02 +0300

    Identity + JWT packages installed; Identity enabled (DbContext, user, migrations).

--- FinalDish.API/Migrations/20260205203315_Identity.Designer.cs
+++ FinalDish.API/Migrations/20260205203315_Identity.Designer.cs
@@ -4,6 +4,7 @@ using FinalDish.API.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FinalDish.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260205203315_Identity")]
+    partial class Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
