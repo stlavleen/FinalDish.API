2dadcf64a4d6d7bfbb07fb4801619f6f0e493ce3
stlavleen <stanislav.zaynullin@outlook.com>
2026-02-16 23:47:32 +0300

    Roles names added

---
+++ FinalDish.API/Constants/RolesNames.cs
@@ -0,0 +1,8 @@
+﻿namespace FinalDish.API.Constants
+{
+    public class RolesNames
+    {
+        public const string Moderator = "Moderator";
+        public const string Admin = "Administrator";
+    }
+}
